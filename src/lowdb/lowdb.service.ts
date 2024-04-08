f7bd301bc532c38317f14f8d953e4c2a43076167
axi92 <axi92@users.noreply.github.com>
2024-04-08 11:52:12 +0200

    database implement find

--- src/lowdb/lowdb.service.ts
+++ src/lowdb/lowdb.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
-// import { JSONFilePreset } from 'lowdb/node'
-// import { Low } from 'lowdb/lib';
-import {v4 as uuidv4} from 'uuid';
+
+import { v4 as uuidv4 } from 'uuid';
 import { DataBaseStructure, ServerEntry } from './lowdb.interface.js';
 import { LowWithLodash } from './lowWithLodash.js';
 import { JSONFile } from 'lowdb/node';
@@ -31,32 +30,36 @@ export class LowdbService {
     }
     this.logger.log('DB initiated!');
     // TODO: move this to tests:
-    // const sampleData : ServerEntry = {
-    //   owner: 123,
-    //   privateid: uuidv4(),
-    //   publicid: uuidv4(),
-    //   notes: 'blub notes'
-    // }
-    // await this.add(sampleData, collctionName);
+    // await this.add(123, 'blub notes', collctionName);
+
+    // TODO: move this to tests:
+    // const findResult = await this.find({ owner: 123}, collctionName)
+    // this.logger.log(findResult);
   }
 
-  // TODO: WORK IN PROGRESS add
-  async add(record: any, collctionName: CollctionName): Promise<any> {
+  async add(owner: number, notes: string, collctionName: CollctionName): Promise<any> {
     const listData = this.db.chain.get(collctionName).value();
-    listData.push(record);
+    this.logger.log(listData);
+    const newEntry: ServerEntry = {
+      owner: owner,
+      privateid: uuidv4(),
+      publicid: uuidv4(),
+      notes: notes
+    }
+    listData.push(newEntry);
     this.db.chain.set(collctionName, listData);
     await this.db.write();
-    return record;
+    return newEntry;
+  }
+
+  async find(condition: object, collctionName: CollctionName): Promise<any> {
+    const values = this.db.chain.get(collctionName).find(condition).value();
+    return values;
   }
 
   // async findAll(collctionName: CollctionName): Promise<any> {
   //   const listUsers = this.db.chain.get(collctionName).value();
   //   return listUsers;
-  // }
-
-  // async find(condition: object, collctionName: CollctionName): Promise<any> {
-  //   const values = await this.db.chain.get(collctionName).find(condition).value();
-  //   return values;
   // }
 
   // async update(
