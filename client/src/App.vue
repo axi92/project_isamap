8c400a8173ea7393216f4f1eca5d39612484c6a9
axi92 <axi92@users.noreply.github.com>
2025-05-29 17:13:47 +0200

    feat(client)!: toolchain upgrade, new event service,
    BREAKING CHANGES:
    - overall toolchain upgrade for client
    - moved to ts instead of js for new stuff
    - migrate sass with migration tool to not use @import because its deprecated (https://sass-lang.com/documentation/breaking-changes/import/)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup lang="ts">
+import { inject } from 'vue';
+import { EventService } from './event/event.service';
+import socket from '@/ws/socket'
+import { EventType, type EventResponse } from './event/event.interface';
+
+const es = inject<EventService>('es')!
+// TODO: make ws work
+
+socket.connect()
+</script>
 
 <template>
   <router-view />
