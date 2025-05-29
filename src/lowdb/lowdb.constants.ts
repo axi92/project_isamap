d8f59d593caa525be6cb2bf8c3ab0b6907f13079
axi92 <axi92@users.noreply.github.com>
2025-05-29 11:58:32 +0200

    test: done on lowdb

--- src/lowdb/lowdb.constants.ts
+++ src/lowdb/lowdb.constants.ts
@@ -1,3 +1,5 @@
 import { DataBaseStructure } from "./lowdb.interface";
 
 export const defaultData: DataBaseStructure = { users: [], servers: [] };
+export const ERROR_USER_EXISTS = 'User already exists!'
+export const WARN_SAVING_DB_SHUTDOWN = 'Saving databases on shutdown!'
