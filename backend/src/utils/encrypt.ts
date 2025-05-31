eb45ac7abaaa37bf4e41c4289aca2028f040f437
axi92 <axi92@users.noreply.github.com>
2025-05-31 18:19:57 +0200

    style: moved from tslint to eslint

--- backend/src/utils/encrypt.ts
+++ backend/src/utils/encrypt.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as CryptoJS from "crypto-js";
 
-export function encrypt(token: string): String {
+export function encrypt(token: string): string {
   //return CryptoJS.AES.encrypt(token, process.env.SECRET_PASSPHRASE).toString();
   return "TBD"
 }
 
-export function decrypt(token: string): String {
+export function decrypt(token: string): string {
   //return CryptoJS.AES.decrypt(token, process.env.SECRET_PASSPHRASE).toString();
   return "TBD"
 }
