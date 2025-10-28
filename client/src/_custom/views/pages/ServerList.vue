5965c7c76647912f80c6025eb7f57eb6597a034c
axi92 <axi92@users.noreply.github.com>
2025-10-28 23:13:08 +0100

    feat(client-serverlist): add default server asa logo

--- client/src/_custom/views/pages/ServerList.vue
+++ client/src/_custom/views/pages/ServerList.vue
@@ -69,7 +69,7 @@ async function handleCreateServer() {
           <div v-for="(item, index) in slotProps.items" :key="index">
             <div class="flex flex-col sm:flex-row sm:items-center p-6 gap-4" :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
               <div class="md:w-40 relative">
-                <img class="block xl:block mx-auto rounded w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" />
+                <img class="block xl:block mx-auto rounded w-full" :src="`/images/logo/ASA_Logo_transparent.png`" :alt="item.name" />
                 <div class="absolute bg-black/70 rounded-border" style="left: 4px; top: 4px">
                   <!-- <Tag :value="item.inventoryStatus" :severity="warn"></Tag> -->
                 </div>
