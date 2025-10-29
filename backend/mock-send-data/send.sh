0b1e783e79981ac13257ea7ec19abc7df1f44107
axi92 <axi92@users.noreply.github.com>
2025-10-29 22:20:42 +0100

    test(backend): add first mock data to send via script

---
+++ backend/mock-send-data/send.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# Backend URL
+URL="http://localhost:3000/api/v1/servers/data"
+
+# If specific files are passed as arguments, use those.
+# Otherwise, default to all .json files in the current directory.
+FILES=${@:-*.json}
+
+for DATA_FILE in $FILES; do
+  echo "Sending data from $DATA_FILE to $URL..."
+
+  curl -X POST "$URL" \
+    -H "Content-Type: application/json" \
+    -d @"$DATA_FILE" \
+    -w "\nHTTP Status: %{http_code}\n" \
+    -s -o >(jq '.')
+
+done
