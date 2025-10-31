c049d4f800e8b593688bfa807b9e0824c74878b7
axi92 <axi92@users.noreply.github.com>
2025-10-31 21:37:29 +0100

    feat(client): add map properties
    Center, Scorched, Ragnarok

--- client/src/_custom/service/map/mapService.constants.ts
+++ client/src/_custom/service/map/mapService.constants.ts
@@ -1,8 +1,14 @@
 import type { MapProperty } from './map.interface';
-import { TheIsland_WP } from './properties/theIslandWp';
+import { Ragnarok_WP } from './properties/Ragnarok_WP';
+import { ScorchedEarth_WP } from './properties/ScorchedEarth_WP';
+import { TheCenter_WP } from './properties/TheCenter_WP';
+import { TheIsland_WP } from './properties/TheIsland_WP';
 
 export const mapProperties = {
   TheIsland_WP: TheIsland_WP,
+  TheCenter_WP: TheCenter_WP,
+  ScorchedEarth_WP: ScorchedEarth_WP,
+  Ragnarok_WP: Ragnarok_WP,
 } as const satisfies Record<string, MapProperty>;
 
 export type MapKey = keyof typeof mapProperties;
---
+++ client/src/_custom/service/map/properties/TheIsland_WP.ts
@@ -0,0 +1,34 @@
+import type { MapProperty } from '../map.interface';
+
+export const TheIsland_WP: MapProperty = {
+  name: 'TheIsland_WP',
+  displayName: 'The Island',
+  bounds: [
+    [0, 0],
+    [100, 100],
+  ],
+  mapSrc: '/images/maps/TheIsland_WP.jpg',
+  obelisks: [
+    {
+      description: 'Blue Obelisk',
+      color: 'blue',
+      src: '/images/maps/obelisk_blue.png',
+      x: 25.5,
+      y: 25.6,
+    },
+    {
+      description: 'Green Obelisk',
+      color: 'green',
+      src: '/images/maps/obelisk_green.png',
+      x: 59,
+      y: 72.3,
+    },
+    {
+      description: 'Red Obelisk',
+      color: 'red',
+      src: '/images/maps/obelisk_red.png',
+      x: 79.8,
+      y: 17.4,
+    },
+  ],
+};
