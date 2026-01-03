fa5dba996ed8cdc5695e0b7985f7fc94182dd8ba
axi92 <axi92@users.noreply.github.com>
2026-01-03 18:22:46 +0100

    feat(client): add git sha to footer

---
+++ client/src/env.d.ts
@@ -0,0 +1,2 @@
+// Defined via vite in vite.config.mjs
+declare const __GIT_COMMIT__: string;
--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -5,9 +5,15 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { execSync } from 'node:child_process';
+
+const commitHash = execSync('git rev-parse HEAD').toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __GIT_COMMIT__: JSON.stringify(commitHash),
+  },
   optimizeDeps: {
     noDiscovery: true,
   },
