91fccf67a31a48fcfa5ac5478ac924fb35f08416
axi92 <axi92@users.noreply.github.com>
2026-01-03 01:22:05 +0100

    style(client): make map icons more responsive for smaller screens

--- client/src/_custom/views/pages/ServerList.vue
+++ client/src/_custom/views/pages/ServerList.vue
@@ -117,7 +117,7 @@ const confirmDelete = (event: Event, publicId: string) => {
         <div class="flex flex-col">
           <div v-for="(item, index) in slotProps.items" :key="index">
             <div class="flex flex-col sm:flex-row sm:items-center p-6 gap-4" :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
-              <div class="md:w-40 relative">
+              <div class="w-24 sm:w-40 relative">
                 <img class="block xl:block mx-auto rounded w-full" :src="`/images/logo/${resolveMapLogo(item.map)}`" :alt="item.name" />
               </div>
               <div class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6">
