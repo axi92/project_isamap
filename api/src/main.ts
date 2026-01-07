b94b8d0975504430ee599f90b773d42895195327
axi92 <axi92@users.noreply.github.com>
2026-01-07 20:06:24 +0100

    chore(api): removed not used import

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigurationService } from './configuration/configuration.service';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
