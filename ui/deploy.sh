c7439b04706cc2d2de42bf1a390c9746001c6d89
axi92 <axi92@users.noreply.github.com>
2026-01-07 10:10:04 +0100

    ci(ui): add build-arg for sha

--- ui/deploy.sh
+++ ui/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 export COMMIT_SHA=$(git rev-parse HEAD)
 docker compose down
-docker compose build --no-cache
+docker compose build --no-cache --build-arg COMMIT_SHA=$COMMIT_SHA
 docker compose up -d
